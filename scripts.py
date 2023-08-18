d4baff0f4e90fd0ac198798e52139d5d99e3c9ed
solo-nb <to.solo.nb@gmail.com>
2023-08-18 16:56:04 +0500

    Raplaced [0] with first()

--- scripts.py
+++ scripts.py
@@ -12,7 +12,7 @@ def get_schoolkid(name_schoolkid: str) -> Schoolkid:
         print('Найдено несколько учеников с таким именем')
         return
 
-    return schoolkids[0]
+    return schoolkids.first()
 
 
 def fix_marks(name_schoolkid: str, points=4):
@@ -22,6 +22,7 @@ def fix_marks(name_schoolkid: str, points=4):
 
     marks_count = Mark.objects.filter(
         schoolkid=schoolkid, points__lte=3).update(points=points)
+
     print(f'Изменено {marks_count} оценок')
 
 
@@ -52,11 +53,10 @@ def get_last_lesson(name_lesson: str, schoolkid: Schoolkid) -> Lesson:
     lessons = Lesson.objects.filter(
         year_of_study=schoolkid.year_of_study,
         group_letter=schoolkid.group_letter,
-        subject=subjects[0]
+        subject=subjects.first()
     ).order_by('-date')
 
-    if lessons.count() > 0:
-        return lessons[0]
+    return lessons.first()
 
 
 def create_commendation(name_schoolkid: str, name_lesson: str,
